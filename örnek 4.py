74c357fc5c4d2c04c329f564756a7eb1bfc7ad3c
sevdesevi <svsila16@gmail.com>
2026-06-06 23:37:07 +0300

    Update örnek 4.py

--- örnek 4.py
+++ örnek 4.py
@@ -4,8 +4,8 @@ b=random.randint(1,10)
 if a%2==0 and b%2==0:
     print("Tüm Sayılar Çift")
 elif a%2!=0 and b%2!=0:
-    print("Tüm Sayılar Tektir")    #BURAK ŞAHİN
-elif a%2==0 and b%2!=0:            #TARAFINDAN KODLANDI
+    print("Tüm Sayılar Tektir")    
+elif a%2==0 and b%2!=0:            
     print("A Çift B Tektir")
 elif a%2!=0 and b%2==0:
     print("A Tek B Çifttir")
