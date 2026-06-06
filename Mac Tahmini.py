74a8486e53221503e10eeeee43095fc93fed3d67
sevdesevi <svsila16@gmail.com>
2026-06-06 23:36:43 +0300

    Update Mac Tahmini.py

--- Mac Tahmini.py
+++ Mac Tahmini.py
@@ -2,7 +2,7 @@ import random
 zar1=random.randint(1,6)
 zar2=random.randint(1,6)
 if zar1%2==0 and zar2%2==0:
-    print("Fenerbahçe")         #Burak Şahin Tarafından Kodlandı
+    print("Fenerbahçe")
 elif zar1%2!=0 and zar2%2!=0:
     print("Beşiktaş")
 else:
